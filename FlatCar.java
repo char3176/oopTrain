33da4551054924fd45d020512bf840a837136612
char@odin <char@odin>
2020-04-21 23:09:28 -0400

    initial flatcar definition

--- FlatCar.java
+++ FlatCar.java
@@ -1,1 +1,13 @@
 // flatcar
+public class FlatCar{
+    double carHeight;
+    double carLength;
+    double carWidth;
+    double carWeight;
+
+    double cargoWeight;
+
+    int percentLoaded;
+    int carIndexInTrain;
+
+}
